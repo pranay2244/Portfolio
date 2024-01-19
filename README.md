7c83b337389fda21e2cff5ed1b32491d06b3969a
pranay2244 <pranaykumarbommaraju2000@gmail.com>
2024-01-19 22:09:15 +0530

    New Design

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Creating My Own Portfolio Website
 2. Using Tailwind for CSS Styling
 3. Using CSS for Custom Styling and Fonts
 4. Using JS for implementing functionality
+
+To Run Tailwind
+npx tailwindcss -i main.css -o styles.css --watch
--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 // Toggle dark mode and light mode
 const modeToggleCircleBig = document.getElementById("modeToggleCircleBig");
 const darkModeTogglerBig = document.getElementById("darkModeTogglerBig");
+
+const modeToggleCircle = document.getElementById("modeToggleCircle");
+const darkModeToggler = document.getElementById("darkModeToggler");
+
 const body = document.body;
 darkModeTogglerBig.addEventListener("change", () => {
   body.classList.toggle("dark");
   modeToggleCircleBig.classList.toggle("left-0");
+  modeToggleCircle.classList.toggle("left-0");
 });
 
 // Toggle dark mode and light mode
-const modeToggleCircle = document.getElementById("modeToggleCircle");
-const darkModeToggler = document.getElementById("darkModeToggler");
+
 darkModeToggler.addEventListener("change", () => {
   body.classList.toggle("dark");
   modeToggleCircle.classList.toggle("left-0");
+  modeToggleCircleBig.classList.toggle("left-0");
 });
 
 function navDisplay() {
   const smallNavMenuItems = document.querySelector(".smallNavMenuItems");
   const smallNavIcon = document.querySelector(".small-nav-icon");
-  console.log(smallNavMenuItems, smallNavIcon);
+  smallNavMenuItems.classList.toggle("hidden");
   smallNavIcon.classList.toggle("fa-ellipsis-v");
   smallNavIcon.classList.toggle("fa-times");
-  smallNavMenuItems.classList.toggle("hidden");
 }
